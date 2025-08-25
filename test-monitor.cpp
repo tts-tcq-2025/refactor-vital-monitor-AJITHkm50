6b9b39da960e11acdc1c1432701cb91d089e54e3
AJITHkm50 <fjt2kor@bosch.com>
2025-08-25 16:24:21 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -3,9 +3,9 @@
 #include "./monitor.h"
 
 void testVitalsOk() {
-    assert(vitalsOk(36.5f, 75.0f, 97.0f) == 1);  // All vitals okay
-    assert(vitalsOk(35.0f, 75.0f, 97.0f) == 0);  // Low temperature
-    assert(vitalsOk(102.5f, 75.0f, 97.0f) == 0); // High temperature
+    assert(vitalsOk(36.5f, 75.0f, 97.0f) == 1);  //  All vitals okay
+    assert(vitalsOk(35.0f, 75.0f, 97.0f) == 0);  //  Low temperature
+    assert(vitalsOk(102.5f, 75.0f, 97.0f) == 0);  //  High temperature
     assert(vitalsOk(36.5f, 55.0f, 97.0f) == 0);  // Low pulse rate
     assert(vitalsOk(36.5f, 105.0f, 97.0f) == 0); // High pulse rate
     assert(vitalsOk(36.5f, 75.0f, 89.0f) == 0);  // Low SpO2
@@ -16,3 +16,4 @@ int main() {
     testVitalsOk();
     return 0;
 }
+
