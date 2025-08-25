cace9a2967b24f0e940cab039dd9ff5080cc5c7e
AJITHkm50 <fjt2kor@bosch.com>
2025-08-25 16:26:09 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -7,7 +7,7 @@ void testVitalsOk() {
     assert(vitalsOk(35.0f, 75.0f, 97.0f) == 0);  //  Low temperature
     assert(vitalsOk(102.5f, 75.0f, 97.0f) == 0);  //  High temperature
     assert(vitalsOk(36.5f, 55.0f, 97.0f) == 0);  // Low pulse rate
-    assert(vitalsOk(36.5f, 105.0f, 97.0f) == 0); // High pulse rate
+    assert(vitalsOk(36.5f, 105.0f, 97.0f) == 0);  // High pulse rate
     assert(vitalsOk(36.5f, 75.0f, 89.0f) == 0);  // Low SpO2
     std::cout << "All vitals tests passed!" << std::endl;  // Use std::endl for flushing
 }
@@ -16,4 +16,5 @@ int main() {
     testVitalsOk();
     return 0;
 }
+
 
