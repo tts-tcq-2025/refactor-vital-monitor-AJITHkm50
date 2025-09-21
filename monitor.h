d967f11f5648796ae83c7aac56524f976af95e9d
AJITHkm50 <fjt2kor@bosch.com>
2025-09-21 23:04:34 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#ifdef __cplusplus
 #include <vector>
 #include <string>
+#endif
 
 // Returns 1 if all vitals are OK, 0 otherwise
 int vitalsOk(float temperature, float pulseRate, float spo2);
 
+#ifdef __cplusplus
 // Returns a vector of warning messages for abnormal vitals
-
 std::vector<std::string> getVitalWarnings(float temperature, float pulseRate, float spo2);
-
+#endif
